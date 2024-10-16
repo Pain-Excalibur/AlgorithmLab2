2c63d51d0675cf401c60cd09a0c22b0ccd909d26
Alexander <itskilobyte@gmail.com>
2024-10-16 21:58:27 +0500

    Initialized the base interface.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Text;
+﻿using OxyPlot.Series;
+using OxyPlot.Wpf;
+using OxyPlot;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using OxyPlot.Axes;
+using System.Printing;
 
 namespace AlgorithmLab2
 {
@@ -19,6 +15,48 @@ namespace AlgorithmLab2
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTimeGraph();
+        }
+
+        private void InitializeTimeGraph()
+        {
+            PlotModel timeModel = new();
+
+            timeModel.Axes.Add(CreateAxis(AxisPosition.Bottom, "Размерность"));
+            timeModel.Axes.Add(CreateAxis(AxisPosition.Left, "Секунды"));
+
+            TimeGraph.Model = timeModel;
+        }
+
+
+        private static LinearAxis CreateAxis(AxisPosition position, string title)
+        {
+            return new LinearAxis
+            {
+                Position = position,
+                AbsoluteMinimum = 0,
+                Title = title
+            };
+        }
+
+        private void RunButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (RecursionSelector.SelectedItem != null)
+            {
+                if (uint.TryParse(InputBox.Text, out uint n))
+                {
+                    // Заглушка
+                    MessageBox.Show("Всё окей дружище.", "Ура", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Введены некорректные параметры, повторите ввод.", "ОШИБКА: НЕКОРРЕКТНЫЕ ПАРАМЕТРЫ", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Перед началом необходимо выбрать тип рекурсии.", "ОШИБКА: НЕ ВЫБРАНА РЕКУРСИЯ", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
